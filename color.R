e10f78fc9b79eb689e71797de07b9898af3c1be0
dong-tak123 <77623014+dong-tak123@users.noreply.github.com>
2021-09-09 21:35:33 +0900

    Add files via upload

---
+++ color.R
@@ -0,0 +1,4 @@
+win.graph(width=4.875, height=2.5, pointsize=8)
+data(color)
+plot(color, ylab='Color Property', xlab='Batch', type='o')
+
---
+++ larain.R
@@ -0,0 +1,5 @@
+library(TSA)
+win.graph(width=4.875, height=2.5, pointsize=8)
+data(larain)
+plot(larain, ylab="Inches", xlab="Year", type='o')
+
